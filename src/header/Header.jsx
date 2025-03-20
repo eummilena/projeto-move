755b1f531254d9480f774a82a4c17a2651cf9d02
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-03-20 16:11:19 -0300

    alteracao no header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import './Header.css'
+import styles from './Header.module.css'
 import logo from '../assets/images/logo-move.png'
 import Button from '../components/Button'
 
 const Header = () => {
   return (
-    <header className='header-bg'>
+    <header className={styles.header}>
+      <div className={styles.align}>
           <img src={logo} alt="logo move"/>
-          <div className='align'>
-            <h1>Mudar-se nunca foi tão fácil antes.</h1>
-          <h4>Ajuda você a planejar, reservar toda a sua mudança do conforto da sua casa.</h4>
-          <Button className='buttonHeader' text="Faça um orçamento"/>
-          </div>
+      <h1 className={ styles.title}>Mudar-se nunca foi tão fácil antes.</h1>
+      <h4 className={ styles.subtitle}>Ajuda você a planejar, reservar toda a sua mudança do conforto da sua casa.</h4>
+        <Button className={styles.buttonHeader} text="Faça um orçamento"/>
+         </div>
     </header>
   )
 }

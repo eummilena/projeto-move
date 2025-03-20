e8d097627262e3fe98a5278fdb8c3f4665dfb8a3
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-03-20 17:42:13 -0300

    ajuste no header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -7,7 +7,8 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.align}>
-          <img src={logo} alt="logo move"/>
+                <img src={logo} alt="logo move"/>
+
       <h1 className={ styles.title}>Mudar-se nunca foi tão fácil antes.</h1>
       <h4 className={ styles.subtitle}>Ajuda você a planejar, reservar toda a sua mudança do conforto da sua casa.</h4>
         <Button className={styles.buttonHeader} text="Faça um orçamento"/>

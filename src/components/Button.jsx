ae6f831abaa32359e33deee9d9c08341868622cf
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-03-20 16:01:21 -0300

    refatoracao botao

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,20 +1,9 @@
 import React from 'react'
-import styled from 'styled-components';
 
-const ButtonMove = styled.button`
- text-transform: uppercase;
- font-size:23px;
- padding:1.5rem;
- border-radius: 50px;
- font-weight: 700;
- border: none;
- width:20%;
- cursor:pointer;
-`;
 
 const Button = ({text, className}) => {
   return (
-      <ButtonMove className={className}>{text}</ButtonMove>
+      <button className={className}>{text}</button>
   )
 }
 

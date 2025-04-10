5be798833a00947c74948c19a201d498da6372d2
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-04-10 11:10:44 -0300

    step1 add css

--- src/form/Step1.jsx
+++ src/form/Step1.jsx
@@ -3,39 +3,39 @@ import styles from './Form.module.css'
 import Input from '../components/Input'
 
 const formStep = [
-        {
-        id: 'nome',
-        label: 'Nome',
-            type:'text',
-        },
-      {
-        id: 'sobrenome',
-        label: 'Sobrenome',
-            type:'text',
-        },
-        {
-        id: 'email',
-        label: 'Email',
-            type:'email',
-        },
-          {
-        id: 'telefone',
-        label: 'Telefone',
-        type:'text',
-    },
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'sobrenome',
+    label: 'Sobrenome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'telefone',
+    label: 'Telefone',
+    type: 'text',
+  },
 ]
 
 
 
 const Step1 = () => {
   return (
-   <div >
-        {formStep.map(({ id, label, type }) => (
-            <div key={id} className={styles.step1}>
-                <label>{label}</label>
-                <Input type={type} id={id} />
-            </div>
-        ))}
+    <div className={styles.step1} >
+      {formStep.map(({ id, label, type }) => (
+        <div key={id} className={`${id === 'nome' && styles.form1} ${id === 'sobrenome' && styles.form2} ${id === 'email' && styles.form3} ${id === 'telefone' && styles.form4} `}>
+          <label htmlFor={id}>{label}</label>
+          <Input type={type} id={id} />
+        </div>
+      ))}
     </div>
   )
 }

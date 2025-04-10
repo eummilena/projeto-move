7015b12e246b0524556c76cef7d1d357f11ff9c3
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-04-10 12:52:54 -0300

    step3 add css

--- src/form/Step3.jsx
+++ src/form/Step3.jsx
@@ -1,16 +1,39 @@
 import React from 'react'
+import styles from './Form.module.css'
+
+
+const formStep3 = [
+  {
+    label: "Endereço para retirada",
+    type: "text",
+    id: "address",
+  },
+  {
+    label: "Cidade",
+    type: "text",
+    id: "cidade",
+  },
+  {
+    label: "Estado",
+    type: "text",
+    id: "estado",
+  },
+  {
+    label: "CEP",
+    type: "text",
+    id: "cep",
+  }
+]
 
 const Step3 = () => {
   return (
-    <div>
-          <label htmlFor="address">Endereço para retirada</label>
-        <input type="text" id="address" />
-        <label htmlFor='cidade'> Cidade</label>
-        <input type="text" id='cidade' />
-        <label htmlFor='estado'> Estado</label>
-        <input type="text" id='estado' />
-        <label htmlFor="cep">CEP</label>
-        <input type="text" id="cep" />
+    <div className={styles.step3}>
+      {formStep3.map(({ label, type, id }) => (
+        <div className={` ${id === 'address' && styles.input1} ${id === 'cidade' && styles.input2} ${id === 'estado' && styles.input3} ${id === 'cep' && styles.input4} `}>
+          <label htmlFor={id}>{label}</label>
+          <input type={type} id={id} />
+        </div>
+      ))}
     </div>
   )
 }

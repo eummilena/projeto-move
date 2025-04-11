a3976b0b7b18cf368759008c95ccc76501148666
Milena Mota <80923819+eummilena@users.noreply.github.com>
2025-04-11 16:00:12 -0300

    up

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 
-const Input = ({type, name, value, onChange,label}) => {
+const Input = ({ type, name, value, onChange, }) => {
   return (
-      <div>
-          <label htmlFor={name}>{label}</label>
-      <input type={type} name={name} value={value} onChange={onChange}/>
+    <div>
+      <input type={type} name={name} value={value} onChange={onChange} required />
     </div>
   )
 }
--- src/form/Step1.jsx
+++ src/form/Step1.jsx
@@ -28,12 +28,16 @@ const formStep = [
 
 
 const Step1 = () => {
+
+
   return (
     <div className={styles.step1} >
       {formStep.map(({ id, label, type }) => (
         <div key={id} className={`${id === 'nome' && styles.form1} ${id === 'sobrenome' && styles.form2} ${id === 'email' && styles.form3} ${id === 'telefone' && styles.form4} `}>
           <label htmlFor={id}>{label}</label>
-          <Input type={type} id={id} />
+          <Input type={type} id={id}
+
+          />
         </div>
       ))}
     </div>
